0f126277999bf0b0efd29d5f57ac1ebc52bb7802
Espress0 <kornstonk@gmail.com>
2023-07-10 10:42:27 +0700

    Draw my first flowchart

--- Lacture01/testgit.py
+++ Lacture01/testgit.py
@@ -3,5 +3,15 @@ print("INE INET Class")
 
 import random
 S = random.randrange(1,100)
+t = int(input("Let's try: "))
 
-print(S)
+while True:
+    
+    if t == S :
+        print("correct")
+    elif t <= S :
+        print("It's less")
+    elif t >= S :
+        print("It's to much")
+    else :
+        print("Wrong")
