28c9b30c34c219e1a22a935890149799456c97fa
Espress0 <kornstonk@gmail.com>
2023-07-10 10:52:23 +0700

    First Draw FlowChart

--- Lacture01/testgit.py
+++ Lacture01/testgit.py
@@ -1,17 +1,17 @@
-print("Hello Korn")
-print("INE INET Class")
+# print("Hello Korn")
+# print("INE INET Class")
 
-import random
-S = random.randrange(1,100)
-t = int(input("Let's try: "))
+# import random
+# S = random.randrange(1,100)
+# t = int(input("Let's try: "))
 
-while True:
+# while True:
     
-    if t == S :
-        print("correct")
-    elif t <= S :
-        print("It's less")
-    elif t >= S :
-        print("It's to much")
-    else :
-        print("Wrong")
+#     if t == S :
+#         print("correct")
+#     elif t <= S :
+#         print("It's less")
+#     elif t >= S :
+#         print("It's to much")
+#     else :
+#         print("Wrong")
