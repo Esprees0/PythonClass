ff2d5afa08ae7b90d79c205d9b3fa17bc0199e4e
Esprees0 <kornstonk@gmail.com>
2023-07-24 22:35:17 +0700

    งง

--- Lacture03/Divide27-30.py
+++ Lacture03/Divide27-30.py
@@ -1,1 +1,12 @@
-print('\t27\t28\t29\t30')
+'''print('KPH \t MPH')
+print("=============")
+for kph in range(60,140,10):
+    mph = float(kph*(0.6214))
+    print(kph, '\t' ,format(mph,'.1f'))
+
+print('______________')'''
+
+print('Devide|\t27\t28\t29\t30')
+for twelve in range(1,13): 
+
+    print(twelve,'\t',twoseven,'\t',twoeieght,'\t',twonine,'\t',threezero)
