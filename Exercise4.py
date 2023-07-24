19325963462a5b477d2dfb49142332bb1f8b5d7e
Espress0 <kornstonk@gmail.com>
2023-07-24 09:17:56 +0700

    lacture02

--- Exercise4.py
+++
@@ -1,1 +0,0 @@
-print("Please select operattion")
---
+++ Lacture02/Exercise4.py
@@ -0,0 +1,10 @@
+print("Please select operattion:")
+"1.Add" 
+"2.Subtract"
+"3.Multiply"
+"4.Divide"
+Select = input("Select operations from 1,2,3,4 :")
+if Select == "1":
+    one = int(input("Enter first number :"))
+    two = int(input("Enter second number :"))
+    print()
