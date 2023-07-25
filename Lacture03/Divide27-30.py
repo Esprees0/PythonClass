624d956a996f0f52a8a59e093938ece646eb0328
Esprees0 <kornstonk@gmail.com>
2023-07-25 13:47:40 +0700

    ทำได้แล้ว

--- Lacture03/Divide27-30.py
+++ Lacture03/Divide27-30.py
@@ -6,7 +6,17 @@ for kph in range(60,140,10):
 
 print('______________')'''
 
-print('Devide|\t27\t28\t29\t30')
+print('Devide\t| 27\t| 28\t| 29\t| 30')
+print("======================================")
+twenty7 = 27
+twenty8 = 28
+twenty9 = 29
+thirty = 30
 for twelve in range(1,13): 
+    twoseven = twenty7*twelve
+    twoeieght = twenty8*twelve
+    twonine = twenty9*twelve
+    threezero = thirty*twelve
+    print(twelve,'\t|',twoseven,'\t|',twoeieght,'\t|',twonine,'\t|',threezero)  
+    print('-------------------------------------')  
 
-    print(twelve,'\t',twoseven,'\t',twoeieght,'\t',twonine,'\t',threezero)
