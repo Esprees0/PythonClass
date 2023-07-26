9ae2467f644989428a8f3a89e9c20c0398818657
Espress0 <kornstonk@gmail.com>
2023-07-26 09:43:03 +0700

    เรียน

--- Lacture03/Divide27-30.py
+++ Lacture03/Divide27-30.py
@@ -1,1 +1,22 @@
-print('\t27\t28\t29\t30')
+'''print('KPH \t MPH')
+print("=============")
+for kph in range(60,140,10):
+    mph = float(kph*(0.6214))
+    print(kph, '\t' ,format(mph,'.1f'))
+
+print('______________')'''
+
+print('Devide\t| 27\t| 28\t| 29\t| 30')
+print("======================================")
+twenty7 = 27
+twenty8 = 28
+twenty9 = 29
+thirty = 30
+for twelve in range(1,13): 
+    twoseven = twenty7*twelve
+    twoeieght = twenty8*twelve
+    twonine = twenty9*twelve
+    threezero = thirty*twelve
+    print(twelve,'\t|',twoseven,'\t|',twoeieght,'\t|',twonine,'\t|',threezero)  
+    print('-------------------------------------')  
+
