677e69bdd85c3cbe5d0759d0ca9602d49f513838
Espress0 <kornstonk@gmail.com>
2023-08-07 11:40:55 +0700

    sleepy

---
+++ Lacture04/Date.py
@@ -0,0 +1,37 @@
+date_string = '1/26/2014'
+date_list = date_string.split('/')
+# if date_list[1] == 1:
+#     date_list[1] = ("Jacnurary")
+# elif date_list[1] == 2 :
+#     date_list[1] = ("Feburary")
+# elif date_list[1] == 3 :
+#     date_list[1] = ("March")
+# elif date_list[1] == 4:
+#     date_list[1] = ("April")
+# elif date_list[1] == 5 :
+#     date_list[1] = ("May")
+# elif date_list[1] == 6 :
+#     date_list[1] = ("June")
+# elif date_list[1] == 7 :
+#     date_list[1] = ("July")
+# elif date_list[1] == 8 :
+#     date_list[1] = ("August")
+# elif date_list[1] == 9 :
+#     date_list[1] = ("September")
+# elif date_list[1] == 10 :
+#     date_list[1] = ("October")
+# elif date_list[1] == 11 :
+#     date_list[1] = ("November")
+# elif date_list[1] == 12 :
+#     date_list[1] = ("December")
+
+# print('Month:', date_list[0])
+print('Day:', date_list[1])
+print('Year:', date_list[2])
+print(date_list)
+
+# for date_list[1] in range(1,13):
+#     date_list[1]
+
+
+
