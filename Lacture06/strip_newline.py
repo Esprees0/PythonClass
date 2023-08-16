c125402d1b5353f9c4281c51072b0cdd2b3ad608
Espress0 <kornstonk@gmail.com>
2023-08-16 09:48:26 +0700

    Sicksick

---
+++ Lacture06/strip_newline.py
@@ -0,0 +1,17 @@
+def main():
+    infile = open('philosophers.txt','r')
+    line1 = infile.readline()
+    line2 = infile.readline()
+    line3 = infile.readline()
+    
+    line1 = line1.strip('\n')
+    line2 = line2.strip('\n')
+    line3 = line3.strip('\n')
+
+    infile.close()
+
+    print(line1)
+    print(line2)
+    print(line3)
+
+main()
