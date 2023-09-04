eb83658cbe12555c1ee9f183874a3d1aada5790e
Esprees0 <kornstonk@gmail.com>
2023-09-04 11:06:55 +0700

    sio

---
+++ Lacture07/Listmethods.py
@@ -0,0 +1,22 @@
+heros = ['Ironman','Thor','Hulk','Superman','Spiderman']
+h2 = ['Dr.Strange','Cpt. America','Black Panther', 'Ant Man']
+
+heros.insert(0,h2[0])
+print(heros.index('Thor'))
+heros.insert(heros.index('Thor'),h2[1])
+print(heros)
+heros.remove('Superman')
+heros.append('Ant Man')
+print(heros)
+heros.sort()
+print(heros)
+heros.reverse()
+print(heros)
+newheros = heros
+newheros[0] = 'Wonder woman'
+print(heros)
+copyheros = [] + heros
+print(copyheros)
+copyheros[0] = 'Hanuman'
+print(heros)
+print(copyheros)
---
+++ Lacture07/listtest.py
@@ -0,0 +1,12 @@
+def findBigestNUm(nums):
+    bigest  = nums[0]
+    for i in nums:
+        if i > bigest:
+            bigest = i
+    return(bigest)
+
+listnum = [4,5,17,3,2,9]
+
+print(findBigestNUm(listnum))
+print(max(listnum))
+print(min(listnum))
