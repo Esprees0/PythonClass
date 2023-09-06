0058a1c96c39f3fac7b5f7970205f1d50b1dfe93
Esprees0 <kornstonk@gmail.com>
2023-09-06 10:33:49 +0700

    nuk

--- Lacture07/Line_chart2.py
+++ Lacture07/Line_chart2.py
@@ -4,7 +4,9 @@ def main ():
     x_coords = [0,1,2,3,4]
     y_coords = [0,3,1,5,2]
 
-    plt.plot(x_coords,y_coords, marker='0')
+    plt.plot(x_coords,y_coords, marker='o')
+
+    plt.title('Sales by Year')
 
     plt.xlabel('Year')
     plt.ylabel('Seal')
@@ -12,7 +14,7 @@ def main ():
     plt.xticks([0,1,2,3,4],
                ['2016','2017','2018','2019','2020'])
     plt.yticks([0,1,2,3,4,5],
-               ['$0n','$1n','$2n','$3n','$4n','$5n'])
+               ['$0m','$1m','$2m','$3m','$4m','$5m'])
 
     plt.grid(True)
 
