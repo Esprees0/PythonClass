815e101214b1c9e6fa5c8c3ccd88c2bf277ee223
Esprees0 <kornstonk@gmail.com>
2023-09-18 10:05:21 +0700

    vn

--- Lacture07/Writecodeheros.py
+++ Lacture07/Writecodeheros.py
@@ -1,50 +1,47 @@
 Listname = ['Ironman','Thor','Hulk','Superman','Spiderman','Dr.Strange','Cpt. America','Black Panther', 'Ant Man']
 Make_loop = 'y'
+def Show_hero(Listname):
+    return Listname
+def addhero(name):
+    return Listname.append(name)
 
-def main():
+def insertheros(index,name):
+    return Listname.insert(index,name)
 
-    def Show_hero(Listname):
-        return Listname
-    def addhero(name):
-        return Listname.append(name)
+def removeheros(name):
+    return Listname.remove(name)
 
-    def insertheros(index,name):
-        return Listname.insert(index,name)
+def Sortedlist_Ascend(Listname):
+    return Listname.sort()
 
-    def removeheros(name):
-        return Listname.remove(name)
+def Sortedlist_Descend(Listname):
+    Listname.sort()
+    return Listname.reverse()
 
-    def Sortedlist_Ascend(Listname):
-        return Listname.sort()
+print("What do you want to do" "\n1.Display hero" "\n2.Add heros" "\n3.Insert heros" "\n4.Renove heros" "\n5.Display Sorted Heros" "\n6.Exit")
 
-    def Sortedlist_Descend(Listname):
-        Listname.sort()
-        return Listname.reverse()
+while Make_loop.lower == 'y':
+    Make_list = int(input("Select :"))
+    if Make_list == 1:
+        Show_hero(Listname)
+    elif Make_list == 2:
+        Select_heros = str(input("Hero you choose :"))
+        addhero(Select_heros)
 
-    print("What do you want to do" "\n1.Display hero" "\n2.Add heros" "\n3.Insert heros" "\n4.Renove heros" "\n5.Display Sorted Heros" "\n6.Exit")
+    elif Make_list == 3:
+        insert_hero_index = int(input("Index you want: "))
+        Select_heros = str(input("Hero you choose :"))
+        insertheros(insert_hero_index,Select_heros)
 
-    while Make_loop.lower() == 'y':
-        Make_list = int(input("Select :"))
-        if Make_list == 1:
-            Show_hero(Listname)
-        elif Make_list == 2:
-            Select_heros = str(input("Hero you choose :"))
-            addhero(Select_heros)
+    elif Make_list == 4:
+        Remove_heros = str(input("Hero you want to renmove: "))
+        removeheros(Remove_heros)
 
-        elif Make_list == 3:
-            insert_hero_index = int(input("Index you want: "))
-            Select_heros = str(input("Hero you choose :"))
-            insertheros(insert_hero_index,Select_heros)
-
-        elif Make_list == 4:
-            Remove_heros = str(input("Hero you want to renmove: "))
-            removeheros(Remove_heros)
-
-        elif Make_list == 5:
-            sorted_hero = str(input("Ascending/Descending (A/D):"))
-            if sorted_hero.upper == "A":
-                Sortedlist_Ascend(Listname)
-            elif sorted_hero.upper == "D":
+    elif Make_list == 5:
+        sorted_hero = str(input("Ascending/Descending (A/D):"))
+        if sorted_hero.upper == "A":
+            Sortedlist_Ascend(Listname)
+        elif sorted_hero.upper == "D":
                 Sortedlist_Descend(Listname)
         elif Make_list == 6:
             break
@@ -52,4 +49,7 @@ def main():
         Make_loop = str(input("Do you want to do again? (y/n):"))
             
 print("The result",Listname)
+
+
+
 
---
+++ Lacture08/Homework.py
@@ -0,0 +1,42 @@
+def maxProductExplanation(n):
+    Multiply = split_Product(n)
+    String_split = ' + '.join(map(str,Multiply))
+    String_Multi = ' × '.join(map(str,Multiply))
+    Find_MultiplyProduct(Multiply)
+    Result = (f'Input: n = {n}\nOutput: {Find_MultiplyProduct(Multiply)}\nExplanation: {n} = {String_split}, {String_Multi} = {Find_MultiplyProduct(Multiply)}')
+    return Result
+
+def split_Product(Product):
+    Split_product = []
+    if Product == 0 or Product == 1:
+        Split_product.append(Product)
+    elif Product <= 5:
+        Split_product.append(Product//2)
+        Split_product.append(Product//2 + Product%2)
+    elif Product > 4:
+        Num = 0
+        for i in range(Product,0,-1):
+            Num += 1
+            if Num == 3:
+                Split_product.append(3)
+                Num = 0
+            elif i == 4:
+                Split_product.append(2)
+                Split_product.append(2)
+                break
+    Split_product.sort()
+    return Split_product
+
+def Find_MultiplyProduct(Product):
+    Mulitply_list = 1 
+    for k in Product:
+        Mulitply_list *= k
+    return Mulitply_list
+
+
+# Example usage
+print(maxProductExplanation(2))
+print(maxProductExplanation(5))
+print(maxProductExplanation(7))
+print(maxProductExplanation(10))
+print(maxProductExplanation(15))
